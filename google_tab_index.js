9c242c1b1d5cdb82fa9af6b3d9946121b19d9f52
tomoaki_mukaiyama <heymanyaman314@gmail.com>
2021-08-02 17:11:53 +0900

    change display none to remove

--- google_tab_index.js
+++ google_tab_index.js
@@ -1,108 +1,133 @@
-javascript:(()=>{
+javascript: (() => {
 
-skipToContent();
-
-function skipToContent(){
-    document.querySelector('[jsname="BKxS1e"]').click();
-};
-document.querySelector("#pnnext").onfocus = function(){
     skipToContent();
-};
 
-if(document.querySelectorAll(".d6cvqb")){
-};
+    function skipToContent() {
+        document.querySelector('[jsname="BKxS1e"]').click();
+    };
+    document.querySelector("#pnnext").onfocus = function () {
+        skipToContent();
+    };
+
+    if (document.querySelectorAll(".d6cvqb")) {
+    };
 
 
-if (document.querySelectorAll(".rGhul.IHSDrd")){
-    var nodes = document.querySelectorAll(".rGhul.IHSDrd");
-    nodes.forEach( node => {
-        node.setAttribute("tabindex","-1");
+    if (document.querySelectorAll(".rGhul.IHSDrd")) {
+        var nodes = document.querySelectorAll(".rGhul.IHSDrd");
+        nodes.forEach(node => {
+            node.setAttribute("tabindex", "-1");
+        });
+    };
+
+    if (document.querySelector("#appbar")) {
+        document.querySelector("#appbar").remove();
+    };
+
+    if (document.querySelector(".Okagcf")) {
+        document.querySelector(".Okagcf").remove()
+    };
+
+    var nodes = document.querySelectorAll('[jscontroller="K6HGfd"]');
+    nodes.forEach(node => {
+        node.remove();
     });
-};
 
-if (document.querySelector("#appbar")){
-    document.querySelector("#appbar").remove();
-};
-
-if (document.querySelector(".Okagcf")){
-    document.querySelector(".Okagcf").remove()
-};
-
-var nodes = document.querySelectorAll('[jscontroller="K6HGfd"]');
-nodes.forEach(node=>{
-    node.remove();
-});
-
-var nodes = document.querySelectorAll(".yuRUbf");
-nodes.forEach( node => {
-    node.firstElementChild.addEventListener('focus', () => {
-        document.activeElement.scrollIntoView({
-            behavior: "auto",
-            block: "center"
+    if (document.querySelectorAll(".fl.iUh30").length){
+        var translateLinks = document.querySelectorAll(".fl.iUh30");
+        translateLinks.forEach( link => {
+            link.remove();
         });
+    };
+
+    var nodes = document.querySelectorAll(".yuRUbf");
+    nodes = Array.from(nodes);
+    nodes.forEach(node => {
+        if (node.querySelector(".GHDvEf")) {
+            node.querySelector(".GHDvEf").remove();
+        };
     });
-    if (node.querySelector(".GHDvEf")) {
-        node.querySelector(".GHDvEf").setAttribute("tabindex","-1");
-    };
-});
 
-document.querySelector(".logo").querySelector("a").setAttribute("tabindex", "-1");
+    document.querySelector(".logo").querySelector("a").remove();
 
-var nodes = document.querySelectorAll(".cUezCb.luh4tb.O9g5cc.uUPGi");
-if (document.querySelectorAll(".cUezCb.luh4tb.O9g5cc.uUPGi")){
-    nodes.forEach( node => {
-    node.style.display = "none";
-});
-};
 
-if (document.querySelector('.Tg7LZd')) {
-    document.querySelector('.Tg7LZd').style.display = "none";
-};
-    
-if (document.querySelector('[jscontroller="unV4T"]')) {
-    document.querySelector('[jscontroller="unV4T"]').style.display = "none";
-};
+    document.querySelector("#rso").addEventListener("keydown", function (event) {
+        if (event.key === "w") {
+            if (前の要素があれば) {
+                前の要素にフォーカス
+                document.activeElement.scrollIntoView({
+                    behavior: "auto",
+                    block: "center"
+                });
+            }
+        } else if (event.key === "s") {
+            if (後の要素があれば) {
+                下の要素にフォーカス
+                document.activeElement.scrollIntoView({
+                    behavior: "auto",
+                    block: "center"
+                });
+            }
+        }
 
-if (document.querySelector('[jscontroller="qik19b"]')) {
-    document.querySelector('[jscontroller="qik19b"]').style.display = "none";
-};
+    });
 
-if (document.querySelectorAll(".Q3DXx")[1]){
-    document.querySelectorAll(".Q3DXx")[1].style.display ="none"
-};
 
-if (document.querySelector(".FyYA1e.IThcWe")){
-    document.querySelector(".FyYA1e.IThcWe").style.display = "none";
-};
-
-if (document.querySelector(".jmjoTe")){
-    document.querySelector(".jmjoTe").style.display = "none";
-};
-
-if (document.querySelectorAll(".HiHjCd")){
-    var nodes = document.querySelectorAll(".HiHjCd");
-    if (nodes) {
-        nodes.forEach( node =>{
-            node.style.display = "none";
+    var nodes = document.querySelectorAll(".cUezCb.luh4tb.O9g5cc.uUPGi");
+    if (document.querySelectorAll(".cUezCb.luh4tb.O9g5cc.uUPGi")) {
+        nodes.forEach(node => {
+            node.remove();
         });
     };
-};
 
-if (document.querySelectorAll(".ULSxyf")){
-    var nodes = document.querySelectorAll(".ULSxyf");
-    if (nodes){
-        nodes.forEach ( node => {
-            node.style.display = "none";
-        });
+    if (document.querySelector('.Tg7LZd')) {
+        document.querySelector('.Tg7LZd').remove();
     };
-};
 
-if (document.querySelectorAll(".fl.iUh30")){
-    var nodes = document.querySelectorAll(".fl.iUh30");
-    nodes.forEach( node => {
-        node.style.display = "none";
-    })
-};
+    if (document.querySelector('[jscontroller="unV4T"]')) {
+        document.querySelector('[jscontroller="unV4T"]').remove();
+    };
+
+    if (document.querySelector('[jscontroller="qik19b"]')) {
+        document.querySelector('[jscontroller="qik19b"]').remove();
+    };
+
+    if (document.querySelectorAll(".Q3DXx")[1]) {
+        document.querySelectorAll(".Q3DXx")[1].remove();
+    };
+
+    if (document.querySelector(".FyYA1e.IThcWe")) {
+        document.querySelector(".FyYA1e.IThcWe").remove();
+    };
+
+    if (document.querySelector(".jmjoTe")) {
+        document.querySelector(".jmjoTe").remove();
+    };
+
+    if (document.querySelectorAll(".HiHjCd")) {
+        var nodes = document.querySelectorAll(".HiHjCd");
+        if (nodes) {
+            nodes.forEach(node => {
+                node.remove();
+            });
+        };
+    };
+
+    if (document.querySelectorAll(".ULSxyf")) {
+        var nodes = document.querySelectorAll(".ULSxyf");
+        if (nodes) {
+            nodes.forEach(node => {
+                node.remove();
+            });
+        };
+    };
+
+    if (document.querySelectorAll(".fl.iUh30")) {
+        var nodes = document.querySelectorAll(".fl.iUh30");
+        nodes.forEach(node => {
+            node.remove();
+        })
+    };
 
 
 
