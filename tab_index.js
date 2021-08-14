29903af9260711fc5401577ff9463efe1c5fb63b
tomoaki_mukaiyama <heymanyaman314@gmail.com>
2021-08-14 10:41:53 +0900

    イベントリスナーの重複修正

--- tab_index.js
+++ tab_index.js
@@ -1,21 +1,58 @@
+skipToContent();
 
-addCssOnce();
-function addCssOnce() {
+function skipToContent() {
+    var nodes = document.querySelectorAll(".yuRUbf");
+    if (nodes.length) {
+        nodes[0].firstElementChild.focus();
+    }
+};
+
+executeOnce();
+function executeOnce() {
     const CLASS_ALREADY_ADDED = "class-already-added";
     var checkStyle = document.querySelector(".srp.wf-b");
     if (checkStyle.classList.contains(CLASS_ALREADY_ADDED)) {
         return;
     } else {
+        //１.３倍ズーム
         checkStyle.classList.add(CLASS_ALREADY_ADDED);
         var newStyleZoom = document.createElement('style');
         newStyleZoom.type = 'text/css';
         newStyleZoom.innerText = 'html{transform-origin: top left; transform: scale(1.3);}';
         document.getElementsByTagName('HEAD').item(0).appendChild(newStyleZoom);
 
+        //フォーカス時の下線
         var newStyleFocus = document.createElement('style');
         newStyleFocus.type = 'text/css';
         newStyleFocus.innerText = 'a:focus{outline: none; border-bottom: 5px solid red; }';
         document.getElementsByTagName('HEAD').item(0).appendChild(newStyleFocus);
+
+        //W＆Sのイベントリスナー
+        var i = 0;
+        document.querySelector("#rso").addEventListener("keydown", function (event) {
+            var nodes = document.querySelectorAll(".yuRUbf");
+            if (event.key === "s") {
+                if (nodes[parseInt(i) + 1]) {
+                    var nextLink = nodes[i + 1].firstElementChild;
+                    nextLink.focus();
+                    nextLink.scrollIntoView({
+                        behavior: "instant",
+                        block: "center"
+                    });
+                    i = i + 1;
+                }
+            } else if (event.key === "w") {
+                if (node = nodes[i - 1]) {
+                    var previousLink = nodes[i - 1].firstElementChild;
+                    previousLink.focus();
+                    previousLink.scrollIntoView({
+                        behavior: "instant",
+                        block: "center"
+                    });
+                    i = i - 1;
+                }
+            };
+        })
     }
 }
 
@@ -116,42 +153,3 @@ if (nodes.length) {
     })
 };
 
-skipToContent();
-
-function skipToContent() {
-    var nodes = document.querySelectorAll(".yuRUbf");
-    if (nodes.length) {
-        nodes[0].firstElementChild.focus();
-    }
-};
-
-var i = 0;
-document.querySelector("#rso").addEventListener("keydown", function (event) {
-    var nodes = document.querySelectorAll(".yuRUbf");
-    if (event.key === "s") {
-        if (nodes[parseInt(i) + 1]) {
-            var nextLink = nodes[i + 1].firstElementChild;
-            nextLink.focus();
-            nextLink.scrollIntoView({
-                behavior: "instant",
-                block: "center"
-            });
-            i = i + 1;
-        }
-    } else if (event.key === "w") {
-        if (node = nodes[i - 1]) {
-            var previousLink = nodes[i - 1].firstElementChild;
-            previousLink.focus();
-            previousLink.scrollIntoView({
-                behavior: "instant",
-                block: "center"
-            });
-            i = i - 1;
-        }
-    };
-})
-
-
-
-//ブックマークレット
-javascript:(function(url){s=document.createElement('script');s.src=url;document.body.appendChild(s);})('https://tomoaki-mukaiyama.github.io/google_bookmarklet/google_search_simplify.js')
